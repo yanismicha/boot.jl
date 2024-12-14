77626fd4d37500737a73711242c60d9ce240c779
Micha Yanis <yanismicha02@gmail.com>
2024-12-14 15:54:44 +0100

    add tests fucntions

--- test/essai.jl
+++ test/essai.jl
@@ -1,16 +1,56 @@
 using Boot
 using DataFrames
 using MLJ
+using Test
 
 ##########################################################################################
 ####################################Test de mes fonctions ################################
 ##########################################################################################
+
+## 1er test: bonjour ##
+function test_bonjour(name::String)
+    @test bonjour(name) == "coucou $name" && bonjour() == "coucou le monde"
+end
+test_bonjour("yanis")
+
+
+## 2eme test: randstring ##
+function test_randstring(possible_chars::Vector{Char}, length_string::Int, size_vector::Int)
+    # Génération des chaînes aléatoires
+    vec = randstring(possible_chars,length_string,size_vector)
+ 
+    @test length(vec) == size_vector
+
+    for s in vec
+        for c in s
+            @test c ∈ possible_chars
+        end
+    end
+end
+
+test_randstring(vcat('A','N'),10,10)
+
+## 3 eme test: bootstrapp ##
 function accuracy(yhat, y)
     return mean(mode.(yhat) .== y)
 end
 function logloss2(yhat,y)
     return StatisticalMeasures.log_loss(yhat,y)
 end
+
+function test_bootstrap(B::Int,constant::Int)
+    data = [1, 2, 3, 4, 5]
+    data2 = fill(constant,100)
+    statistic = std
+    boot_values = bootstrap(data,statistic,B)
+    @test length(boot_values) == B
+    @test all(0.0 <= r <= 2.0 for r in boot_values)
+    statistic = mean
+    boot_values = bootstrap(data2,statistic,B)
+    @test mean(boot_values) == constant
+end
+
+test_bootstrap(10)
 
 function test_boot_MLJ(data::DataFrame;target::Symbol= :target,score::Function = acc)
     
@@ -20,6 +60,7 @@ function test_boot_MLJ(data::DataFrame;target::Symbol= :target,score::Function =
     mach = machine(tree, X, y)
     acc = mean(bootstrap(data,mach,score))
     acc2 = mean(bootstrap(data,tree,score))
+    
     return acc,acc2
 end
 
